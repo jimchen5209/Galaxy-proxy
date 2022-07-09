dc72bb023a8d9225e6308e2bef9470c897b55ccd
jimchen5209 <jimchen5209@gmail.com>
2022-07-09 09:21:47 +0800

    Skip sending resourcePack when same resourcePack is applied

--- src/main/kotlin/one/oktw/galaxy/proxy/resourcepack/ResourcePackHelper.kt
+++ src/main/kotlin/one/oktw/galaxy/proxy/resourcepack/ResourcePackHelper.kt
@@ -6,10 +6,16 @@ import one.oktw.galaxy.proxy.Main.Companion.main
 class ResourcePackHelper {
     companion object{
         fun trySendResourcePack(player: Player, galaxy: String){
-            val lobbyResourcePack = main.config.galaxiesResourpacePack[galaxy] ?: return
+            val resourcePack = main.config.galaxiesResourpacePack[galaxy] ?: return
+
+            // Check Player Applied ResourcePack
+            val currentResourcePack = player.appliedResourcePack
+            if (currentResourcePack != null && currentResourcePack.url.equals(resourcePack.uri.toString()) && currentResourcePack.hash.contentEquals(resourcePack.hash)) return
+
+            // Send ResourcePack
             player.sendResourcePackOffer(
-                main.proxy.createResourcePackBuilder(lobbyResourcePack.uri.toString())
-                    .setHash(lobbyResourcePack.hash)
+                main.proxy.createResourcePackBuilder(resourcePack.uri.toString())
+                    .setHash(resourcePack.hash)
                     .setShouldForce(true)
                     .build()
             )
