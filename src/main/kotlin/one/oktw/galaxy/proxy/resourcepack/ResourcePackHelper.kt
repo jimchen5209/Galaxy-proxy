53c858009b9c0de681f17ca3c847099b6b97a32a
jimchen5209 <jimchen5209@gmail.com>
2022-07-09 09:39:49 +0800

    Revert Skip sending resourcePack when same resourcePack is applied reverts dc72bb02

--- src/main/kotlin/one/oktw/galaxy/proxy/resourcepack/ResourcePackHelper.kt
+++ src/main/kotlin/one/oktw/galaxy/proxy/resourcepack/ResourcePackHelper.kt
@@ -7,12 +7,6 @@ class ResourcePackHelper {
     companion object{
         fun trySendResourcePack(player: Player, galaxy: String){
             val resourcePack = main.config.galaxiesResourpacePack[galaxy] ?: return
-
-            // Check Player Applied ResourcePack
-            val currentResourcePack = player.appliedResourcePack
-            if (currentResourcePack != null && currentResourcePack.url.equals(resourcePack.uri.toString()) && currentResourcePack.hash.contentEquals(resourcePack.hash)) return
-
-            // Send ResourcePack
             player.sendResourcePackOffer(
                 main.proxy.createResourcePackBuilder(resourcePack.uri.toString())
                     .setHash(resourcePack.hash)
