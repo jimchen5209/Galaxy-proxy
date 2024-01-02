b273996835e8aef0ce739947daac02055f35e552
jimchen5209 <jimchen5209@gmail.com>
2024-01-02 15:47:08 +0800

    fix: restore ResourcePack

--- src/main/kotlin/one/oktw/galaxy/proxy/config/ConfigManager.kt
+++ src/main/kotlin/one/oktw/galaxy/proxy/config/ConfigManager.kt
@@ -1,19 +1,17 @@
 package one.oktw.galaxy.proxy.config
 
 import com.google.gson.Gson
-import net.kyori.adventure.resource.ResourcePackInfo
+import kotlinx.coroutines.runBlocking
 import one.oktw.galaxy.proxy.Main.Companion.main
 import one.oktw.galaxy.proxy.config.model.GalaxySpec
 import one.oktw.galaxy.proxy.config.model.ProxyConfig
 import one.oktw.galaxy.proxy.config.model.RedisConfig
+import one.oktw.galaxy.proxy.resourcepack.ResourcePack
 import java.io.InputStream
-import java.net.URI
-import java.nio.charset.StandardCharsets
 import java.nio.file.FileSystems
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
-import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 
 class ConfigManager(private val basePath: Path = Paths.get("config")) {
@@ -24,7 +22,7 @@ class ConfigManager(private val basePath: Path = Paths.get("config")) {
     lateinit var redisConfig: RedisConfig
         private set
     val galaxies = HashMap<String, GalaxySpec>()
-    val galaxiesResourcePack = ConcurrentHashMap<String, ResourcePackInfo>()
+    val galaxiesResourcePack = ConcurrentHashMap<String, ResourcePack>()
 
     init {
         readConfig()
@@ -58,28 +56,12 @@ class ConfigManager(private val basePath: Path = Paths.get("config")) {
                 Files.newBufferedReader(file).use { json ->
                     val galaxyName = file.fileName.toString().substringBeforeLast(".")
                     galaxies[galaxyName] = gson.fromJson(json, GalaxySpec::class.java)
-                    try {
-                        main.logger.info("Loading Resource Pack of $galaxyName")
-                        val packInfo = galaxies[galaxyName]?.let { spec ->
-                            main.logger.info("ResourcePack: ${spec.ResourcePack}")
-                            if (spec.ResourcePack.isNotBlank()) {
-                                try {
-                                    return@let ResourcePackInfo.resourcePackInfo()
-                                        .id(UUID.nameUUIDFromBytes(spec.ResourcePack.toByteArray(StandardCharsets.UTF_8))) // From Minecraft
-                                        .uri(URI(spec.ResourcePack))
-                                        .computeHashAndBuild()
-                                        .join()
-                                } catch (e: Exception) {
-                                    main.logger.error("Error computing hash and building ResourcePackInfo", e)
-                                    return@let null
-                                }
-                            }
-                            return@let null
+                    runBlocking {
+                        try {
+                            galaxiesResourcePack[galaxyName] = galaxies[galaxyName]?.let { spec -> if (spec.ResourcePack.isNotBlank()) ResourcePack.new(spec.ResourcePack) else null } ?: return@runBlocking
+                        } catch (e: Exception) {
+                            main.logger.error("Resource pack load failed!", e)
                         }
-                        main.logger.info("Resource Pack: \n\tID=${galaxiesResourcePack[galaxyName]?.id()}\n\t${galaxiesResourcePack[galaxyName]?.hash()}")
-                        galaxiesResourcePack[galaxyName] = packInfo ?: return
-                    } catch (e: Exception) {
-                        main.logger.error("Resource pack load failed!", e)
                     }
                 }
             }
