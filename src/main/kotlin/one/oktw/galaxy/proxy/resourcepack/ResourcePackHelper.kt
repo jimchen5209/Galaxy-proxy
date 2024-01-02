8ea044c138d910384f810010ab904c1dd95fb971
jimchen5209 <jimchen5209@gmail.com>
2024-01-02 15:47:46 +0800

    fix: use request and uuid

--- src/main/kotlin/one/oktw/galaxy/proxy/resourcepack/ResourcePackHelper.kt
+++ src/main/kotlin/one/oktw/galaxy/proxy/resourcepack/ResourcePackHelper.kt
@@ -1,18 +1,23 @@
 package one.oktw.galaxy.proxy.resourcepack
 
 import com.velocitypowered.api.proxy.Player
+import net.kyori.adventure.resource.ResourcePackRequest
 import one.oktw.galaxy.proxy.Main.Companion.main
 
 class ResourcePackHelper {
-    companion object{
-        fun trySendResourcePack(player: Player, galaxy: String){
+    companion object {
+        fun trySendResourcePack(player: Player, galaxy: String) {
             val resourcePack = main.config.galaxiesResourcePack[galaxy] ?: return
-            player.sendResourcePacks(resourcePack)
+            val request = ResourcePackRequest.resourcePackRequest()
+                .packs(resourcePack.packInfo)
+                .required(true)
+                .build()
+            player.sendResourcePacks(request)
         }
 
         fun tryRemoveResourcePack(player: Player, galaxy: String) {
             val resourcePack = main.config.galaxiesResourcePack[galaxy] ?: return
-            player.removeResourcePacks(resourcePack)
+            player.removeResourcePacks(resourcePack.uuid)
         }
     }
 }
